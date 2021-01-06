cf02ce111c7b693f4e28a840a7acf60088d070a7
JaiVeer-creative <73180396+JaiVeer-creative@users.noreply.github.com>
2021-01-06 19:45:15 +0530

    Create AntArm.js

---
+++ AntArm.js
@@ -0,0 +1,27 @@
+var angle = 0.0;
+var angleDirection = 1;
+var speed = 0.5;
+function setup() {
+angleMode(DEGREES)
+createCanvas(600, 600);
+}
+function draw() {
+background(220);
+translate(300, 300); // Move to start position
+rotate(angle);
+strokeWeight(12);
+line(0, 0, 40, 0);
+translate(40, 0); // Move to next joint
+rotate(angle * 2.0);
+strokeWeight(6);
+  line(0, 0, 30, 0);
+translate(30, 0); // Move to next joint
+rotate(angle * 2.5);
+strokeWeight(3);
+line(0, 0, 20, 0);
+angle += speed * angleDirection;
+if ((angle > 45) || (angle < 0)) {
+//angleDirection *= -1;
+angleDirection=-1*angleDirection
+}
+}
